5af7ad63c876a372e162e4def9c4dcd23dba32c0
Matthijs van den Bos <matthijs@vandenbos.org>
2018-12-09 11:22:28 +0100

    make DHCP default

--- setup-vpn-firewall.sh
+++ setup-vpn-firewall.sh
@@ -1,17 +1,13 @@
 #!/bin/bash -e
 
-# firewall
-# https://www.strongswan.org/docs/LinuxKongress2009-strongswan.pdf
-# https://wiki.strongswan.org/projects/strongswan/wiki/ForwardingAndSplitTunneling
-# https://www.zeitgeist.se/2013/11/26/mtu-woes-in-ipsec-tunnels-how-to-fix/
 set -x
 
-# Config
-USEDHCP="true"
-#VPNDNS="8.8.8.8,8.8.4.4"
-VPNDNS="192.168.2.251"
+function error {
+  echo $1
+  exit 1
+}
 
-[[ $(id -u) -eq 0 ]] || exit_badly "Please re-run as root (e.g. sudo ./path/to/this/script)"
+[[ $(id -u) -eq 0 ]] || error "Please run as root"
 
 # Install required packages
 export DEBIAN_FRONTEND=noninteractive
@@ -20,23 +16,15 @@ export DEBIAN_FRONTEND=noninteractive
 #apt autoremove -y
 #apt-get -o Acquire::ForceIPv4=true install -y strongswan libstrongswan-standard-plugins strongswan-libcharon libcharon-extra-plugins moreutils iptables-persistent
 
+# DNS server used on the internal network
+LOCALDNS=$(dhcpcd -T | grep domain_name_servers | cut -d"=" -f2 | sed -e "s/'//g")
+
 # Network interface used
-ETH0ORSIMILAR=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
+NETINTERFACE=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
 # Internal IP
-IP=$(ifdata -pa $ETH0ORSIMILAR)
-# External IP
-VPNHOST=$(dig +short myip.opendns.com @resolver1.opendns.com)
-
-
-# A pre set IP pool for VPN clients or use DHCP if you have the dhcp plugin for StrongSwan
-VPNIPPOOL="10.10.0.0/16"
-RIGHTSOURCEIP=$VPNIPPOOL
-
-if [[ $USEDHCP = "true" ]]
-then
-	VPNIPPOOL=$(ip route | grep proto | awk -- '{ print $1 }')
-	RIGHTSOURCEIP="%dhcp"
-fi
+IP=$(ifdata -pa $NETINTERFACE)
+EXTERNALIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+SUBNET=$(ip route | grep proto | awk -- '{ print $1 }')
 
 # set the firewall
 iptables -P INPUT   ACCEPT
@@ -60,11 +48,7 @@ iptables -A OUTPUT -o lo -j ACCEPT
 # drop invalid packets
 iptables -A INPUT -m state --state INVALID -j DROP
 
-# rate-limit repeated new requests from same IP to any ports
-#iptables -I INPUT -i $ETH0ORSIMILAR -m state --state NEW -m recent --set
-#iptables -I INPUT -i $ETH0ORSIMILAR -m state --state NEW -m recent --update --seconds 300 --hitcount 60 -j DROP
-
-# allow SSH from our subnet
+# allow SSH
 iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
@@ -73,7 +57,6 @@ iptables -A INPUT -p tcp --destination-port 53 -j ACCEPT
 iptables -A INPUT -p udp --destination-port 53 -j ACCEPT
 iptables -A INPUT -p tcp --destination-port 80 -j ACCEPT
 
-
 # VPN
 
 # accept IPSec/NAT-T for VPN (ESP not needed with forceencaps, as ESP goes inside UDP)
@@ -81,19 +64,18 @@ iptables -A INPUT -p udp --dport  500 -j ACCEPT
 iptables -A INPUT -p udp --dport 4500 -j ACCEPT
 
 # forward VPN traffic anywhere
-iptables -A FORWARD --match policy --pol ipsec --dir in  --proto esp -s $VPNIPPOOL -j ACCEPT
-iptables -A FORWARD --match policy --pol ipsec --dir out --proto esp -d $VPNIPPOOL -j ACCEPT
+iptables -A FORWARD --match policy --pol ipsec --dir in  --proto esp -s $SUBNET -j ACCEPT
+iptables -A FORWARD --match policy --pol ipsec --dir out --proto esp -d $SUBNET -j ACCEPT
 
 # reduce MTU/MSS values for dumb VPN clients
-iptables -t mangle -A FORWARD --match policy --pol ipsec --dir in -s $VPNIPPOOL -o $ETH0ORSIMILAR -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+iptables -t mangle -A FORWARD --match policy --pol ipsec --dir in -s $SUBNET -o $NETINTERFACE -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
 
 # masquerade VPN traffic over eth0 etc.
-iptables -t nat -A POSTROUTING -s $VPNIPPOOL -o $ETH0ORSIMILAR -m policy --pol ipsec --dir out -j ACCEPT  # exempt IPsec traffic from masquerading
-iptables -t nat -A POSTROUTING -s $VPNIPPOOL -o $ETH0ORSIMILAR -j MASQUERADE
+iptables -t nat -A POSTROUTING -s $SUBNET -o $NETINTERFACE -m policy --pol ipsec --dir out -j ACCEPT  # exempt IPsec traffic from masquerading
+iptables -t nat -A POSTROUTING -s $SUBNET -o $NETINTERFACE -j MASQUERADE
 
 
 # fall through to drop any other input and forward traffic
-
 iptables -A INPUT   -j DROP
 iptables -A FORWARD -j DROP
 
@@ -142,13 +124,14 @@ conn roadwarrior
   dpddelay=900s
   rekey=no
   left=%any
-  leftid=${VPNHOST}
+  leftid=${EXTERNALIP}
   leftsubnet=0.0.0.0/0
   leftauth=psk
   right=%any
   rightid=%any
-  rightdns=${VPNDNS}
-  rightsourceip=${VPNIPPOOL}
+  rightdns=${LOCALDNS}
+  rightsourceip=%dhcp
+  #rightsourceip=${SUBNET}
   rightauth=psk
   rightauth2=xauth
 " > /etc/ipsec.conf
